9369e15c6370e61eb3dff2ceb6d91754109b7ed8
Tran Nhat Duy <42115291+hitonichi@users.noreply.github.com>
2023-10-25 13:20:09 +0700

    feat: impl toast service

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,15 @@
+import { ToastService } from '../services/ToastService';
+
 const Homepage = () => (
   <div>
     <h1>This is the Home Page</h1>
+    <button
+      onClick={() => {
+        ToastService.createToast({ title: 'homepage toast' });
+      }}
+    >
+      Generate Toast
+    </button>
   </div>
 );
 
