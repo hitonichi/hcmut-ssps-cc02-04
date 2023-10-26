f9359cdc827641e62992010f524cf5073b6a0cb4
Tran Nhat Duy <42115291+hitonichi@users.noreply.github.com>
2023-10-26 14:10:59 +0700

    feat: update user data for mapping

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,16 +4,31 @@ import { ToastService } from '../services/ToastService';
 const LoginPage = () => {
   const { login } = useAuth();
   return (
-    <div>
+    <div className="flex flex-col gap-2">
       <h1>This is the Login Page</h1>
       <button
+        className="w-[50%] rounded-md bg-slate-200 p-2 hover:bg-slate-400"
         onClick={async () => {
           console.log(login);
-          await login('hehe');
+          await login({
+            role: 'student',
+            uid: '1234567',
+            name: 'Nguyễn Văn A',
+          });
           ToastService.createToast({ title: 'login toast' });
         }}
       >
-        Sign In
+        Sign In As Student
+      </button>
+      <button
+        className="w-[50%] rounded-md bg-slate-200 p-2 hover:bg-slate-400"
+        onClick={async () => {
+          console.log(login);
+          await login({ role: 'spso', uid: '7654321', name: 'Trần Văn B' });
+          ToastService.createToast({ title: 'login toast' });
+        }}
+      >
+        Sign In As SPSO
       </button>
     </div>
   );
