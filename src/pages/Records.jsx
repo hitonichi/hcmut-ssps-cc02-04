1b1841f16612f4b7e19cebc5cefab7c6218111be
John Nguyen <john12052003@gmail.com>
2023-10-29 18:31:42 +0700

    .

--- src/pages/Records.jsx
+++ src/pages/Records.jsx
@@ -87,7 +87,7 @@ const Records = () => {
       </div>
       <div className="roboto mx-4 flex h-[85px] w-[500px] flex-col bg-secondaryContainer">
         <div className="flex h-1/2 w-full flex-row bg-customBlue text-base font-bold text-white">
-          <div className="flex w-[200px] items-center justify-start py-2 pl-5  ">
+          <div className="flex w-[200px] items-center justify-start py-2 pl-4  ">
             Khổ giấy
           </div>
           <div className="flex h-full w-[100px] items-center justify-center ">
@@ -102,7 +102,7 @@ const Records = () => {
         </div>
         <div className="flex h-1/2 w-full flex-row text-base font-bold text-customBlue">
           {' '}
-          <div className="flex w-[200px] items-center justify-start py-2 pl-5  ">
+          <div className="flex w-[200px] items-center justify-start py-2 pl-4  ">
             Tổng số trang đã in{' '}
           </div>
           <div className=" flex h-full w-[100px] items-center justify-center  ">
