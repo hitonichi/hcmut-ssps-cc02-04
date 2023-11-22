29228f48f3fef90835a1eb1e4a9f27e7e51aab6e
pvhn.pvhn@gmail.com <pvhn.pvhn@gmail.com>
2023-11-22 21:34:09 +0700

    Updated policy

--- src/pages/Policies.jsx
+++ src/pages/Policies.jsx
@@ -1,48 +1,77 @@
 // import { useLocation } from 'react-router';
 import PropTypes from 'prop-types';
 import { Button } from '@mui/material';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import {useSearchParams } from 'react-router-dom';
 import AllowedFormatForm from '../components/policy/AllowedFormatForm';
 import AllocationDateForm from '../components/policy/AllocationDateForm';
 import AllocationPageForm from '../components/policy/AllocationPaperForm';
+import paper from '../assets/icon/paper.png';
+import calender from '../assets/icon/calendar.png';
+import format from '../assets/icon/format.png';
 
 const PoliciesPage = () => {
   // const location = useLocation();
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
 
   const renderBody = (type) => {
     switch (type) {
       case 'date':
         //  TODO: Implement the form body here
-        return <AllocationDateForm />;
+        return (<PoliciesContainer><AllocationDateForm /></PoliciesContainer>);
       case 'paper':
         //  TODO: Implement the form body here
-        return <AllocationPageForm />;
+        return (<PoliciesContainer> <AllocationPageForm /> </PoliciesContainer>);
       case 'format':
         //  TODO: Implement the form body here
-        return <AllowedFormatForm />;
+        return (<PoliciesContainer>  <AllowedFormatForm /> </PoliciesContainer>);
       default:
         return (
           <>
+          <div className='min-h-screen flex flex-col items-center justify-center align-center space-y-[40px]'>
             <Button onClick={() => setSearchParams({ type: 'date' })}>
-              Modify allocation date
+              <div className='flex items-center justify-center 
+              bg-blue-200 hover:bg-blue-300 
+              h-[100px] w-[320px] rounded-[20px]
+              space-x-[8px]' >
+                <img className='h-[50px] w-1/8' src={calender} alt="" />
+                <div className='text-black text-base font-medium' >
+                  Modify allocation date
+                </div>
+              </div>
             </Button>
             <Button onClick={() => setSearchParams({ type: 'paper' })}>
-              Modify default allocated paper
+              <div className='flex items-center justify-center 
+              bg-blue-200 hover:bg-blue-300 
+              h-[100px] w-[320px] rounded-[20px]
+              space-x-[8px]' >
+                <img className='h-[50px] w-1/8' src={paper} alt="" />
+                <div className='text-black text-base font-medium' >
+                  Modify allocation date
+                </div>
+              </div>
             </Button>
             <Button onClick={() => setSearchParams({ type: 'format' })}>
-              Modify allowed formats
+              <div className='flex items-center justify-center 
+              bg-blue-200 hover:bg-blue-300 
+              h-[100px] w-[320px] rounded-[20px]
+              space-x-[8px]' >
+                <img className='h-[50px] w-1/8' src={format} alt="" />
+                <div className='text-black text-base font-medium' >
+                  Modify allocation date
+                </div>
+              </div>
             </Button>
+          </div>
           </>
         );
     }
   };
 
   return (
-    <PoliciesContainer>
-      {renderBody(searchParams.get('type'))}
-    </PoliciesContainer>
+    
+      <>{renderBody(searchParams.get('type'))}</>
+    
   );
 };
 
@@ -50,7 +79,7 @@ const PoliciesContainer = ({ children }) => {
   // TODO: implement the layout here
   return (
     <div className="flex h-[100vh] w-full items-center justify-center bg-yellow-100">
-      <div className=" bg-red-100">{children}hehe</div>
+      <div className=" bg-red-100">{children}</div>
     </div>
   );
 };
