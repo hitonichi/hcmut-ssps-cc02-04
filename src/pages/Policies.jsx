1eada9db1ba54faae702c8aa2d05eb3725e7096a
pvhn.pvhn@gmail.com <pvhn.pvhn@gmail.com>
2023-11-27 22:44:34 +0700

    chang the color of the choices

--- src/pages/Policies.jsx
+++ src/pages/Policies.jsx
@@ -5,9 +5,13 @@ import {useNavigate, useSearchParams } from 'react-router-dom';
 import AllowedFormatForm from '../components/policy/AllowedFormatForm';
 import AllocationDateForm from '../components/policy/AllocationDateForm';
 import AllocationPageForm from '../components/policy/AllocationPaperForm';
-import paper from '../assets/icon/paper.png';
-import calender from '../assets/icon/calendar.png';
-import format from '../assets/icon/format.png';
+// import paper from '../assets/icon/paper.png';
+// import calender from '../assets/icon/calendar.png';
+// import format from '../assets/icon/format.png';
+
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import FolderCopyIcon from '@mui/icons-material/FolderCopy';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 const PoliciesPage = () => {
   // const location = useLocation();
@@ -28,39 +32,41 @@ const PoliciesPage = () => {
       default:
         return (
           <>
-          <div className='min-h-screen flex flex-col items-center justify-center align-center space-y-[40px] '>
+          <div className='min-h-screen flex flex-col items-center justify-center align-center space-y-[40px] bg-primaryContainer '>
             <Button onClick={() => setSearchParams({ type: 'date' })}>
-              <div className='grid grid-cols-4 
-              bg-blue-200 hover:bg-blue-300 
-              h-[100px] w-[396px] rounded-[20px]' >
+              <div className='grid grid-cols-4
+              bg-customBlue hover:bg-blue-300 
+              h-[100px] w-[396px] rounded-[20px] text-white' >
                 <div className='flex justify-center items-center '>
-                  <img className='h-[50px] w-[50px] ' src={calender} alt="" />
+                  <CalendarMonthIcon/>
+                  {/* <img className='h-[50px] w-[50px] ' src={calender} alt="" /> */}
                 </div>
-                <div className='col-span-2 flex justify-self-start items-center text-black text-sm font-semibold' >
+                <div className='col-span-2 flex justify-self-start items-center text-sm font-semibold' >
                   Thay đổi ngày phân bố
                 </div>
               </div>
             </Button>
             <Button onClick={() => setSearchParams({ type: 'paper' })}>
               <div className='grid grid-cols-4
-              bg-blue-200 hover:bg-blue-300 
-              h-[100px] w-[396px] rounded-[20px]' >
+              bg-customBlue hover:bg-blue-300 
+              h-[100px] w-[396px] rounded-[20px] text-white' >
                 <div className='flex justify-center items-center '>
-                  <img className='h-[50px] w-[50px] ' src={paper} alt="" />
-                </div>
-                <div className='col-span-3 flex justify-self-start items-center text-black text-sm font-semibold' >
+                  {/* <img className='h-[50px] w-[50px] ' src={paper} alt="" /> */}
+                  <FileCopyIcon/>
+                </div> 
+                <div className='col-span-3 flex justify-self-start items-center text-sm font-semibold' >
                   Thay đổi số trang mặc định
                 </div>
               </div>
             </Button>
             <Button onClick={() => setSearchParams({ type: 'format' })}>
               <div className='grid grid-cols-4 
-              bg-blue-200 hover:bg-blue-300 
-              h-[100px] w-[396px] rounded-[20px]' >
+              bg-customBlue hover:bg-blue-300 
+              h-[100px] w-[396px] rounded-[20px] text-white' >
                 <div className='flex justify-center items-center '>
-                  <img className='h-[50px] w-[50px] ' src={format} alt="" />
+                  <FolderCopyIcon/>
                 </div>
-                <div className='col-span-3 flex justify-self-start items-center text-black text-sm font-semibold'  >
+                <div className='col-span-3 flex justify-self-start items-center  text-sm font-semibold'  >
                   Thay đổi định dạng file được phép
                 </div>
               </div>
@@ -91,7 +97,7 @@ const PoliciesContainer = ({ children, title }) => {
         {children}
         {/* Button back using navigate back router dom */}
         <button onClick={() => navigate("/dashboard/policies")}
-                className='border-[1px] border-customBlue text-customBlue py-[8px] px-[16px] rounded-[20px] font-semibold mt-[450px] mr-[900px]'>
+                className='border-[1px] border-customBlue text-customBlue py-[8px] px-[16px] rounded-[20px] font-semibold mt-[450px] mr-[910px] w-[120px] h-[40px]'>
           Quay lại
         </button>      
       </div>
