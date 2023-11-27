273ddce0a18eb8df04707e7a9fe226ba528b8f3d
Tran Nhat Duy <42115291+hitonichi@users.noreply.github.com>
2023-11-27 16:10:57 +0700

    feat: switch to redux for auth, remove useAuth hook

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,30 +1,67 @@
+import { Link } from '@mui/material';
 import { useAuth } from '../hooks/auth';
 import { ToastService } from '../services/ToastService';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../store/slices/authSlice';
 
 const LoginPage = () => {
   const { login } = useAuth();
+  const dispatch = useDispatch();
   return (
-    <div className="flex flex-col gap-2">
-      <h1>This is the Login Page</h1>
+    <div className="flex flex-col items-center gap-2 p-4">
+      {/* <a href="http://localhost:8080/auth/google" ></a> */}
+      <Link className="w-[50%]" href="http://localhost:8080/auth/google">
+        <button
+          className="w-full rounded-md bg-customBlue p-2 text-white hover:bg-primaryContainer"
+          // onClick={async () => {
+          //   console.log(login);
+          //   await login(
+          //     {>
+          //       role: 'student',
+          //       uid: '1234567',
+          //       name: 'Nguyễn Văn A',
+          //     },
+          //     'local',
+          //   );
+          //   ToastService.createToast({ title: 'login toast' });
+          // }}
+        >
+          Sign In with SSO
+        </button>
+      </Link>
       <button
-        className="w-[50%] rounded-md bg-slate-200 p-2 hover:bg-slate-400"
+        className="w-[50%] rounded-md bg-customBlue p-2 text-white hover:bg-primaryContainer"
         onClick={async () => {
           console.log(login);
-          await login({
-            role: 'student',
-            uid: '1234567',
-            name: 'Nguyễn Văn A',
-          });
+          dispatch(
+            setUser({
+              role: 'student',
+              orgId: '1234567',
+              name: 'Nguyễn Văn A',
+              local: true,
+            }),
+          );
           ToastService.createToast({ title: 'login toast' });
         }}
       >
         Sign In As Student
       </button>
       <button
-        className="w-[50%] rounded-md bg-slate-200 p-2 hover:bg-slate-400"
+        className="w-[50%] rounded-md bg-customBlue p-2 text-white hover:bg-primaryContainer"
         onClick={async () => {
           console.log(login);
-          await login({ role: 'spso', uid: '7654321', name: 'Trần Văn B' });
+          // await login(
+          //   { role: 'spso', uid: '7654321', name: 'Trần Văn B' },
+          //   'local',
+          // );
+          dispatch(
+            setUser({
+              role: 'spso',
+              orgId: '7654321',
+              name: 'Trần Văn B',
+              local: true,
+            }),
+          );
           ToastService.createToast({ title: 'login toast' });
         }}
       >
