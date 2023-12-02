3a0f004f4008c281b000fd5eb82138f355b066eb
Tran Nhat Duy <42115291+hitonichi@users.noreply.github.com>
2023-12-02 22:19:27 +0700

    fix: table alignment

--- src/pages/PrinterManagement.jsx
+++ src/pages/PrinterManagement.jsx
@@ -41,7 +41,7 @@ const PrinterManangement = () => {
   return (
     <div
       style={{ width: widthValue }}
-      className="flex h-screen flex-col gap-[30px] overflow-hidden bg-primaryContainer py-[50px] pl-[40px] pr-[60px]"
+      className="flex h-screen flex-col gap-[30px] overflow-hidden bg-primaryContainer py-[50px] pl-[40px] pr-[40px]"
     >
       <div className="flex h-[60px] w-full flex-row justify-between ">
         <h2 className="roboto ml-8 w-auto text-5xl font-bold text-black">
