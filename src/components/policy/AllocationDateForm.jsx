8244760f362742808bcbabeea69c49266db71711
5/9 <dangquocminh2017@gmail.com>
2023-12-03 14:10:26 +0700

    State complete

--- src/components/policy/AllocationDateForm.jsx
+++ src/components/policy/AllocationDateForm.jsx
@@ -1,30 +1,35 @@
 //import * as React from 'react';
 import TextField from '@mui/material/TextField';
+import {useState} from 'react';
 import Box from '@mui/material/Box';
-import Autocomplete from '@mui/material/Autocomplete';
 import { ThemeProvider, createTheme } from '@mui/material';
 //import { create } from '@mui/material/styles/createTransitions';
 //import { styled } from '@mui/system';
 //import React from 'react';
 import { Button } from '@mui/material';
+import MenuItem from '@mui/material/MenuItem';
 //import { useState } from "react";
-
-const dateInputRange = Array.from(Array(31), (_,i)=>i+1);
-const monthInputRange = Array.from(Array(12), (_,i)=>i+1);
-const arrayRange = (start, stop, step) =>
-    Array.from(
-    { length: (stop - start) / step + 1 },
-    (_, index) => start + index * step
-    );
-const yearInputRange = arrayRange(2023,2030,1).reverse();
-
+import InputAdornment from '@mui/material/InputAdornment';
+import EventRoundedIcon from '@mui/icons-material/EventRounded';
+const weekInputRange = Array.from(Array(15), (_,i)=>i+1);
 const AllocationDateForm = () => {
+  const [week, setWeek] = useState(2);
+  const [newWeek, setNewWeek] = useState(week);
   const theme = createTheme({
     components: {
+      
       MuiOutlinedInput: {
         styleOverrides: {
           root: {
-            
+            height: "43px",
+            paddingLeft : "10px"
+          }
+        }
+      },
+      MuiButtonBase: {
+        styleOverrides: {
+          root: {
+            width: "70px"
           }
         }
       },
@@ -36,75 +41,94 @@ const AllocationDateForm = () => {
         },
         styleOverrides: {
           root: {
-            width: "17%",
+            width: "100px",
             color: 'white',
             backgroundColor:  '#0461a3',
             borderRadius: "30px",
-            height: "65%",
-            fontWeight: '650',
-            fontSize: '11px',
+            height: "30px",
+            fontWeight: '600',
+            fontSize: '14px',
             lineHeight: '18px'
           }
         }
       }
     }
   });
-  const date = {
-    options: dateInputRange
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e)
+    setNewWeek((week));
+    //const result = {week}
+    //console.log(e.target.value)
   }
-  const month = {
-    options: monthInputRange
-  }
-  const year = {
-    options: yearInputRange
-  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
       <div className = 'h-full w-full flex flex-col'>
-        <div className='h-1/2 w-full flex justify-center items-end'>
+        <div className='h-full w-full flex justify-center items-center'>
           
-          <Box component="form" className = 'w-2/3 grid grid-cols-3 gap-0'>
+          <Box 
+            component="form" 
+            onSubmit = {handleSubmit}
             
-            <Autocomplete
-              {...date}
-              renderInput = {(params) =>(
-                
-                <TextField 
-                
-                {...params} type="number" label="Ngày" variant = "outlined" 
-                />
-                
-              )}
-            />
-            <Autocomplete
-              {...month}
-              
-              renderInput = {(params) =>(
-                <TextField {...params} type="number" label="Tháng" variant = "outlined"/>
-              )}
-            />
-            <Autocomplete
-              {...year}
+            className = 'w-full flex flex-col h-2/3 gap-0 justify-center'
+            noValidate
+            autoComplete="off"
+            sx = {
+              {
+                '& .MuiTextField-root': {width: '12ch',textAlign: 'center',fontSize: '14.7px'}
+              }
+            }
+          >
             
-              renderInput = {(params) =>(
-                <TextField {...params} type="number" label="Năm" variant = "outlined"/>
-              )}
-            />
+            <div className ='w-full h-1/3 flex flex-row gap-[1rem] justify-center items-center mt-[80px]'>
+              <div className = 'flex justify-center items-center font-semibold text-[24px] mb-[4px]'>
+                Tuần phân bố mới:
+              </div>
+              <div className = 'flex justify-center items-center'>
+                <TextField
+                  required
+                  select
+                  value = {week}
+                  InputProps={{
+                    startAdornment :(
+                      <InputAdornment position="start">
+                        <EventRoundedIcon/>
+                      </InputAdornment>
+                    )
+                  }}
+                  
+                  //onSubmit = {handleSubmit}
+                  onChange = {(e) => setWeek(e.target.value)}
+                >
+                {weekInputRange.map((option) => (
+                  <MenuItem key = {option} value = {option}>
+                    {option}
+                  </MenuItem>
+                ))}
+            </TextField>
+              </div>
+            </div>
+            <div className = "flex flex-col h-2/3 w-full items-center justify-center mt-[0px]">
+              <div className = "h-1/3 w-full flex justify-center items-center font-['Arial'] text-[16px] italic	mb-[0px]">
+                Số trang mặc định sẽ được cấp cho tài khoản sinh viên vào tuần được chọn ở hai học kì chính
+              </div>
+              <div className = "flex h-1/3 w-full items-center justify-center mt-[35px] h-1/5 font-semibold text-[#f09c21] text-[19.5px]">
             
+                Tuần phân bố trước đó : {newWeek}
+          
+              </div>
+              <div className = "h-1/3 mt-[15px] flex items-top justify-center" >
+                <Button type = "submit"  variant = "contained">
+                  Lưu
+                </Button>
+              </div>
+            </div>
           </Box>
           
         </div>
-        <div className = "flex items-end justify-center mt-[10px] h-1/5 font-semibold text-[#f09c21] text-[18px]">
-            
-            Ngày phân bố trước đó: 25/12/2022
-            
-        </div>
-        <div className = "h-[10%] mt-[25px] flex items-top justify-center" >
-          <Button variant = "contained">
-              Lưu
-          </Button>
-        </div>
+        
       </div>
       </ThemeProvider>
       
